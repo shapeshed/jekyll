413e9d54b350479050b756f52a0abaf149d691e4
George Ornbo <george@lewis.local>
2009-10-08 08:28:55 +0100

    adding alpha category support

--- lib/jekyll/converters/expressionengine.rb
+++ lib/jekyll/converters/expressionengine.rb
@@ -16,12 +16,14 @@ module Jekyll
     # post in wp_posts that has post_status = 'publish'.
     # This restriction is made because 'draft' posts are not guaranteed to
     # have valid dates.
-    QUERY = "SELECT t.title, t.url_title, t.entry_date, d.field_id_1, d.field_id_2
-    FROM exp_weblog_titles as t 
-    LEFT JOIN exp_weblog_data as d
-    ON d.entry_id = t.entry_id
-    WHERE t.status = 'Open'
-    AND t.weblog_id = '1'"
+    QUERY = "SELECT t.title, t.entry_id, t.url_title, t.entry_date, d.field_id_1, d.field_id_2
+        FROM exp_weblog_titles as t 
+        LEFT JOIN exp_weblog_data as d
+        ON d.entry_id = t.entry_id
+        WHERE t.status = 'Open'
+        AND t.weblog_id = '1'"
+        
+
 
     def self.process(dbname, user, pass, host = 'localhost')
       db = Sequel.mysql(dbname, :user => user, :password => pass, :host => host)
@@ -29,6 +31,25 @@ module Jekyll
       FileUtils.mkdir_p "_posts"
 
       db[QUERY].each do |post|
+        
+        post_categories = Array.new
+        jekyll_categories = ""
+        
+        categories = db["SELECT c.cat_name FROM exp_category_posts as p
+        LEFT JOIN exp_categories as c
+        ON p.cat_id = c.cat_id
+        WHERE p.entry_id = #{post[:entry_id]}"]
+        
+
+        
+        categories.each do |category|
+          post_categories << category[:cat_name]
+        end
+        
+        post_categories.each do |post_category|
+          jekyll_categories << post_category
+        end
+        
         # Get required fields and construct Jekyll compatible name
         title = post[:title]
         slug = post[:url_title]
@@ -42,6 +63,7 @@ module Jekyll
         data = {
            'layout' => 'post',
            'title' => title.to_s,
+           'categories' => jekyll_categories,
            'excerpt' => post[:field_id_1].to_s,
          }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
 
